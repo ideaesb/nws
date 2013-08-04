f95ebcaf6441d4f3438c2d0bd2bf9ec18c97883c
IDEA ESB <ideaesb@gmail.com>
2013-08-04 00:32:31 -1000

    First Baby Step -
    CAP is buried (showing up as) foreign markup.  Just need to dig one
    level to get to the VTEC etc.

--- src/org/pdc/nws/National.java
+++ src/org/pdc/nws/National.java
@@ -2,6 +2,18 @@ package org.pdc.nws;
 
 import java.net.*;
 import java.io.*;
+import java.util.*;
+
+import com.google.publicalerts.cap.Alert;
+import com.google.publicalerts.cap.CapException;
+import com.google.publicalerts.cap.NotCapException;
+import com.google.publicalerts.cap.CapException.Reason;
+import com.google.publicalerts.cap.feed.*;
+import com.sun.syndication.feed.module.Module;
+import com.sun.syndication.feed.synd.*;
+
+import org.jdom.Element;
+
 
 
 
@@ -26,7 +38,7 @@ public class National {
 		int connectTimeoutinMilliseconds = 1000;
 		int readTimeoutinMilliseconds = 1000;
 		
-		
+
 		try
 		{
           url = new URL("http://alerts.weather.gov/cap/us.php?x=0");
@@ -36,9 +48,55 @@ public class National {
           conn.setReadTimeout(readTimeoutinMilliseconds);
           BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
           
+          /* echo
           while ((inputLine = in.readLine()) != null) {
         	  out(inputLine);
           }
+          */
+          
+          // construct cap feed parser with boolean validate
+          CapFeedParser atomSmashher = new CapFeedParser(true); 
+          
+          SyndFeed feed = atomSmashher.parseFeed(in);
+	      
+          List<SyndEntry> entries = feed.getEntries();
+          //out("Size = " + entries.size());
+		  List<Alert> alerts = new ArrayList<Alert>();
+          
+		  for (SyndEntry entry : entries)
+		  {
+			  System.out.println(entry.getTitle());
+				System.out.println(entry.getUri());
+				System.out.println(entry.getDescription().getValue());
+				List<Element> foreignMarkups = (List<Element>) entry.getForeignMarkup();
+				for (Element foreignMarkup : foreignMarkups) {
+					
+					System.out.println("Foreign markup "
+							+ foreignMarkup.getNamespaceURI() + "/"
+							+ foreignMarkup.getName() + " = "
+							+ foreignMarkup.getText());
+                    
+					// look one level below
+					for (Element child : (List<Element>) foreignMarkup.getChildren())
+					{
+						System.out.println("Child markup "
+								+ child.getNamespaceURI() + "/"
+								+ child.getName() + " = "
+								+ child.getText());
+					}
+					
+					//out(foreignMarkup.getText());
+				}
+				System.out.println("-----------------");
+
+		  }          
+          
+		  
+		  for (Iterator i = alerts.iterator(); i.hasNext();) {
+             Alert alert = (Alert) i.next();
+             System.out.println(alert.getNote());
+          }
+          
           in.close();
 		}
 		catch (Exception e)
